8dce7004ed3ffa08c4be5ae55a57705954453063
Ben Kiboma <omayioben@gmail.com>
2022-06-08 20:54:17 +0300

    Add watch-all

--- user.test.js
+++ user.test.js
@@ -2,12 +2,15 @@ const User = require('./user');
 
 describe('user >', () => {
   test('test name', () => {
-    const user = new User()
-    expect(user.greeting()).toBe("Hello Stranger");
+    const user = new User() //Arrange
+    let result = user.greeting() //Act
+    expect(result).toBe("Hello Stranger"); //Assert
   });
   
   test('test name', () => {
-    const user = new User()
-    expect(user.greeting('John')).toBe("Hello John");
+    const user = new User() //Arrange
+    let name = 'John' //Arrange
+    let result = user.greeting(name) //Act
+    expect(result).toBe("Hello John"); //Assert
   });
 });
